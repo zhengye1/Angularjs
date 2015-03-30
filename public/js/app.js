656ca4b3dabed4f3baece3d033982a048780163d
zhengye1 <10zhengy@utsc.utoronto.ca>
2015-03-30 01:27:41 -0400

    Update a home page view, will continue when I wake up

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,33 @@
-var app = angular.module('eatz', ['store-products'])
-				 .controller('StoreController', ['$http', function($http){
-					var store = this;
-                    store.products = [];
-                    $http.get('/public/js/models/products.json').success(function(data){
-                        store.products = data;
-                    });
-				 }])
-				 .controller('ReviewController', function(){
-					this.review = {};
-					this.addReview = function(product){
-						this.review.createdOn = Date.now();
-						product.reviews.push(this.review);
-						this.review = {};
-					};
-				 });
+'use strict';
+
+var app = angular.module('eatz', ['ngRoute', 'store-products'])
+    .config(['$routeProvider',
+        function($routeProvider){
+            $routeProvider.when('/', {
+                templateUrl: 'tpl/home.html'
+            })
+                .when('/products', {
+                    templateUrl:'tpl/product-list.html'
+                })
+                .otherwise({
+                    templateUrl:'tpl/404.html'
+                });
+        }])
+    .controller('StoreController', ['$http',function($http){
+        var store = this;
+        store.products = [];
+        $http.get('/public/js/models/products.json').success(function(data){
+            store.products = data;
+            store.products.orderProp = 'price';
+        });
+    }])
+    .controller('ReviewController', function(){
+        this.review = {};
+        this.addReview = function(product){
+            this.review.createdOn = Date.now();
+            product.reviews.push(this.review);
+            this.review = {};
+        };
+    });
 
 
