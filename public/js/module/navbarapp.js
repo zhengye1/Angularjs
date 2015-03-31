2212354dfdfce1baaa24c01b4eee2fb82ee88627
zhengye1 <10zhengy@utsc.utoronto.ca>
2015-03-30 20:41:47 -0400

    Update the nav bar, will try to make it nicer tonight

--- public/js/module/navbarapp.js
+++ public/js/module/navbarapp.js
@@ -8,14 +8,14 @@ angular.module('navbarapp', [])
             replace: true,         // (3)
             transclude: true,      // (4)
             templateUrl: "tpl/header.html",    // (5)
-            compile: function($element, attrs) {
+            compile: function(element, attrs) {
                 var li, liElements, links, index, length;
-
-                liElements = $element.find("#navigation-tabs li");
+                liElements = $(element).find("#bs-example-navbar-collapse-1 li");
                 for (index = 0, length = liElements.length; index < length; index++) {
                     li = liElements[index];
                     links = $(li).find("a");
-                    if (links[0].textContent === attrs.currentTab) $(li).addClass("active");
+                    if ($(links[0]).textContent === attrs.currentTab)
+                        $(li).addClass("active");
                 }
             }
         }})
